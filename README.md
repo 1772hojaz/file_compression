bbfc2f9f789dbd5860648e146cbea2c752ad2a89
Humphrey <h.nyahoja@alustudent.com>
2024-02-08 11:11:11 -0800

    corrected main.py

--- README.md
+++ README.md
@@ -2,4 +2,9 @@ compressing my file to tar
 
 To run it 
 
-~$ python main.py
+
+filecompression# ./main.py 
+Enter the folder name: yes
+Folder exists
+File compressed successfully
+File moved to the folder successfully
--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 #!/usr/bin/python3
 import tarfile
 import os
+import datetime
+import shutil
+
 
 # Extract the tar file
+def select_folder():
+    folder = input("Enter the folder name: ")
+    if os.path.exists(folder):
+        print("Folder exists")
+    else:
+        print("Folder does not exist")
+        folder = select_folder()
+    return folder
 
-def compress(filename,output_filename):
+def compress(folder):
+    date = datetime.datetime.now().strftime("%Y-%m-%d")
+    output_filename = folder + "_" + date + ".tar.gz"
     with tarfile.open(output_filename, "w:gz") as tar:
-        tar.add(filename, arcname=os.path.basename(filename))
-    print("Compressed")
+        tar.add(folder, arcname=os.path.basename(folder))
+    print("File compressed successfully")
+    return output_filename
 
-filename = "test"
-output_filename = "test.tar.gz"
-compress(filename,output_filename)
 
+def main():
+    folder = select_folder()
+    output_filename = compress(folder)
+    shutil.move(output_filename, folder)
+    print("File moved to the folder successfully")
+
+if __name__ == "__main__":
+    main()
